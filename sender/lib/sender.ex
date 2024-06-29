2d7c6be64225667d16bcdc00463e5999fd17bc69
alde103 <aldebaran_103@hotmail.com>
2024-06-28 22:47:18 -0600

    Chapter 1. Easy Concurrency with the Task Module

---
+++ sender/lib/sender.ex
@@ -0,0 +1,36 @@
+defmodule Sender do
+  @moduledoc """
+  Documentation for `Sender`.
+  """
+
+  def send_email("konnichiwa@world.com" = email),
+    do: raise("Oops, couldn't send email to #{email}!")
+
+  def send_email(email) do
+    Process.sleep(3000)
+    IO.puts("Email to #{email} sent")
+    {:ok, "email_sent"}
+  end
+
+  def notify_all(emails) do
+    emails
+    |> Enum.map(fn email ->
+      Task.async(fn ->
+        send_email(email)
+      end)
+    end)
+    |> Enum.map(&Task.await/1)
+  end
+
+  def stream_notify_all(emails) do
+    emails
+    |> Task.async_stream(&send_email/1, on_timeout: :kill_task)
+    |> Enum.to_list()
+  end
+
+  def supervised_notify_all(emails) do
+    Sender.EmailTaskSupervisor
+    |> Task.Supervisor.async_stream_nolink(emails, &send_email/1)
+    |> Enum.to_list()
+  end
+end
