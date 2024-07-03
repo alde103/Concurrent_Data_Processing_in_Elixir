51bb92ddc3048c98aa30c71bec1e25e47c971561
alde103 <aldebaran_103@hotmail.com>
2024-07-02 22:11:20 -0600

    Building Your Data-Processing Pipeline

---
+++ scraper/lib/scraper/page_consumer.ex
@@ -0,0 +1,31 @@
+defmodule PageConsumer do
+  use GenStage
+  require Logger
+
+  def start_link(_args) do
+    initial_state = []
+    GenStage.start_link(__MODULE__, initial_state)
+  end
+
+  # def init(initial_state) do
+  #   Logger.info("PageConsumer init")
+  #   {:consumer, initial_state, subscribe_to: [PageProducer]}
+  # end
+
+  def init(initial_state) do
+    Logger.info("PageConsumer init")
+    #sub_opts = [{PageProducer, min_demand: 0, max_demand: 3}]
+    sub_opts = [{PageProducer, min_demand: 0, max_demand: 3}]
+    {:consumer, initial_state, subscribe_to: sub_opts}
+  end
+
+  def handle_events(events, from, state) do
+    Logger.info("PageConsumer received #{inspect({events, from})}")
+    # Pretending that we're scraping web pages.
+    Enum.each(events, fn _page ->
+      Scraper.work()
+    end)
+
+    {:noreply, [], state}
+  end
+end
